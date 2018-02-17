afd6854b1628b623666d6f1cb22cf87c3f601994
DE KERMADEC Adrien <adrien.dekermadec@e-i.com>
2018-02-17 19:51:13 +0100

    Ajout code JD

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bracoo
+Bracoo project:
+A REST API made with .Net core 2.0 
+This web application is highly portable and can run on Linux, Windows and MacOS 
---
+++ bracoo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using bracoo.Models;
+
+namespace bracoo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Basic application for a POC.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Contact info will be provided later.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
